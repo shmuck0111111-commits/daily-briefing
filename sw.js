b2f828226fc1fe9e1a77f43726169d8281e4467c
shmuck0111111-commits <shmuck0111111-commits@users.noreply.github.com>
2026-03-01 21:37:43 +0000

    deploy: 5298cfccaa7a526a62695802bff77a5e348196c9

---
+++ sw.js
@@ -0,0 +1,11 @@
+
+const CACHE = "briefing-v1";
+const ASSETS = ["./"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
